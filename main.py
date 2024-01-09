23f256b735e9aa453fbccb6ebccb66544278be6f
huxiaoou <516984451@qq.com>
2024-01-09 22:15:41 +0800

    Add instrument_info_path as an argument

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ argument_parser = argparse.ArgumentParser(description="A viewer")
 argument_parser.add_argument("--src", type=str, help="position file path")
 argument_parser.add_argument("--account", type=str, help="TQ-SDK Account")
 argument_parser.add_argument("--password", type=str, help="TQ-SDK Password")
+argument_parser.add_argument("--info", type=str, help="Instrument Info Path")
 args = argument_parser.parse_args()
 
-instru_info_tab_path = r"E:\Deploy\Data\Futures\InstrumentInfo3.csv"
-instru_info_tab = CInstrumentInfoTable(instru_info_tab_path, file_type="CSV")
+instru_info_tab = CInstrumentInfoTable(args.info, file_type="CSV")
 
 mgr_viewer = CManagerViewer(position_file_path=args.src, instru_info_tab=instru_info_tab)
 mgr_viewer.main(args.account, args.password)
