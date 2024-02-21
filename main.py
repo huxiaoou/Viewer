7438a20863ffa2371cf2d35c83f0c1e6e999f4ff
huxiaoou <516984451@qq.com>
2024-02-21 09:11:52 +0800

    Add an argument to control config file

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ argument_parser.add_argument("--src", type=str, help="position file path")
 argument_parser.add_argument("--account", type=str, help="TQ-SDK Account")
 argument_parser.add_argument("--password", type=str, help="TQ-SDK Password")
 argument_parser.add_argument("--info", type=str, help="Instrument Info Path")
+argument_parser.add_argument("--config", type=str, help="config file path")
 args = argument_parser.parse_args()
-config_path = r"viewer.ini"
 
 instru_info_tab = CInstrumentInfoTable(args.info, file_type="CSV")
 
-mgr_viewer = CManagerViewer(position_file_path=args.src, config_path=config_path, instru_info_tab=instru_info_tab)
+mgr_viewer = CManagerViewer(position_file_path=args.src, config_path=args.config, instru_info_tab=instru_info_tab)
 mgr_viewer.main(args.account, args.password)
